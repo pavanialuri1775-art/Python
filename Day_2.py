b8f55139f3d9856c6171c78197924b9313521689
V kids <pavanialuri1775@gmail.com>
2026-01-19 23:39:13 +0530

    added Day_2.py

---
+++ Day_2.py
@@ -0,0 +1,13 @@
+#variable
+#it is a name given to a memory location to store data.
+#ex:
+x=10
+name="Pavani"
+
+#problem
+name="Pavani"
+age=21
+marks=85
+print("name:",name)
+print("age:",age)
+print("age:",age)
