ed388fa4e5798f98853551363aa5b5049e62de55
V kids <pavanialuri1775@gmail.com>
2026-01-23 06:38:57 +0530

    Added day4 problems

---
+++ day4/problems.py
@@ -0,0 +1,37 @@
+#Convert an integer to a string and print its type.
+num=23
+name=str(num)
+print(name)
+print(type(name))
+
+#Convert a string "25" to an integer and add 10.
+age="25"
+n=int(age)
+print(n+10)
+
+#adding two diff datatypes
+a=10
+b=2.5
+print(a+int(b))
+
+x="10"
+y=5
+print(x+str(y)) # +means concatenation for string
+
+#Convert an integer to a complex number.
+x=5
+y=complex(x)
+print(y)
+
+#Print the real and imaginary parts of a complex number
+z=2+3j
+print(z.real)
+print(z.imag)
+
+#converting to boolean
+n=print(bool(0))
+m=print(bool(1))
+o=print(bool(-5))
+p=print(bool(""))
+print(bool("python"))
+
