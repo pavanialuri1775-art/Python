864f13021aa7139572ad71b610c943f4bd62c390
V kids <pavanialuri1775@gmail.com>
2026-01-24 22:16:33 +0530

    Added day6 concatenation programs

---
+++ day6/concatenation.py
@@ -0,0 +1,12 @@
+#string Concatenation:in python to combine or concatenate two strings we use "+" operator
+#example
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+#To add a space between them, add a " ":
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
---
+++ day6/methods.py
@@ -0,0 +1,32 @@
+#Python has a set of built-in methods that you can use on strings.
+#Upper Case:The upper() method returns the string in upper case
+#example:
+a = "Hello, World!"
+print(a.upper()) #HELLO, WORLD!
+
+#Lower Case:The lower() method returns the string in lower case
+a = "Hello, World!"
+print(a.lower()) #hello, world!
+
+#strip():The strip() method removes any whitespace from the beginning or the end
+a = " Hello,World! "
+print(a.strip()) #Hello,World!
+
+#Replace():The replace() method replaces a string with another string
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+#Split():The split() method splits the string into substrings if it finds instances of the separator:
+a = "Hello,World!"
+print(a.split(",")) 
+
+
+
+
+
+
+
+
+
+
+
