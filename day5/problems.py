09e0e3ed0bd32814fe6c5626328a751689bb0ebe
V kids <pavanialuri1775@gmail.com>
2026-01-24 19:54:34 +0530

    Added day5 problems programs

---
+++ day5/problems.py
@@ -0,0 +1,44 @@
+#Print first 5 characters
+s = "programming"
+print(s[0:5])
+
+#Print last 4 characters
+s = "education"
+print(s[-4:])
+
+#Print string except first character
+s = "python"
+print(s[1:])
+
+#Print string except last character
+s = "python"
+print(s[:5])
+
+#Print characters at even index positions
+s = "abcdef"
+print(s[0:5:2])
+
+#Print characters at odd index positions
+s = "abcdef"
+print(s[1:6:2])
+
+#Skip every 2nd character
+s = "learning"
+print(s[0:8:2])
+
+#Print last character
+s = "python"
+print(s[-1])
+
+#Print string from second last character to start
+s = "hello"
+print(s[-2::-1])
+
+#Reverse first 4 characters
+s = "education"
+print(s[3::-1])
+
+
+
+
+
