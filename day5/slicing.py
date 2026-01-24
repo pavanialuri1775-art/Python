1a6e8adf1bc374e1d1942fde41ce0ff7efc3a0d8
V kids <pavanialuri1775@gmail.com>
2026-01-24 16:35:32 +0530

    Added day5 slicing programs

---
+++ day5/slicing.py
@@ -0,0 +1,42 @@
+#slicing:extracting a part of string using index positions.
+#string[start : end : step]
+#start:index to start
+#end:index to stop
+#step:how many steps to jump 
+
+#example:
+s="pavani"
+print(s[0:3]) #pav
+print(s[2:5]) #van
+
+#Slice From the Start
+#When we don’t give the start index, Python automatically starts from the first character (index 0).
+s="pavani"
+print(s[:5])
+
+s="hello world"
+print(s[:5])
+
+#slice to the end
+#When we don’t give the end index, Python will take characters till the end of the string.
+s="pavani"
+print(s[2:])
+
+b = "Hello, World!"
+print(b[7:])
+
+#negative indexing:negative indexing means counting characters from the end of the string.
+s="pavani"
+print(s[-5:-1])
+
+b = "Hello, World!"
+print(b[-6:-1])
+
+
+
+
+
+
+
+
+
