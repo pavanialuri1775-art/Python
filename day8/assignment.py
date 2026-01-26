1004a1ab878f7cc971d0f256a102234b96668bb1
V kids <pavanialuri1775@gmail.com>
2026-01-26 23:18:36 +0530

    Added  day8  programs

---
+++ day8/assignment.py
@@ -0,0 +1,95 @@
+#Assignment operators:Assignment operators are used to assign values to variables
+#Simple assignment
+x = 10
+print(x)
+
+#Assign multiple values
+a = 5
+b = a
+print(a, b)
+
+#ADDITION ASSIGNMENT (+=)
+#*Add and assign
+x = 10
+x += 5
+print(x)
+
+#With float
+x = 4.5
+x += 1.5
+print(x)
+
+#With string
+s = "Py"
+s += "thon"
+print(s)
+
+#SUBTRACTION ASSIGNMENT (-=)
+#Subtract and assign
+x = 20
+x -= 7
+print(x)
+
+#With float
+x = 10.5
+x -= 2.5
+print(x)
+
+#MULTIPLICATION ASSIGNMENT (*=)
+#Multiply and assign
+x = 6
+x *= 3
+print(x)
+
+#With string
+s = "Hi "
+s *= 3
+print(s)
+
+#Boolean with *=
+x = True
+x *= 5
+print(x)
+
+#DIVISION ASSIGNMENT (/=)
+#Divide and assign
+x = 20
+x /= 4
+print(x)
+print(type(x))
+
+#Division with float
+x = 7
+x /= 2
+print(x)
+
+#FLOOR DIVISION ASSIGNMENT (//=)
+x = 17
+x //= 3
+print(x)
+
+x = -17
+x //= 3
+print(x)
+
+#MODULUS ASSIGNMENT (%=)
+#Modulus and assign
+x = 14
+x %= 2
+print(x)
+
+#POWER ASSIGNMENT (**=)
+#Power and assign
+x = 2
+x **= 3
+print(x)
+
+#
+s="5"
+s+="7"
+print(s)
+
+
+
+
+
---
+++ day8/operators.py
@@ -0,0 +1,64 @@
+#operators
+# "+" operator is used to add together two values
+#Example
+print(5+6)
+
+#it can also be used to add together a variable and a value, or two variables:
+sum1 = 100 + 50      # 150 (100 + 50)
+sum2 = sum1 + 250    # 400 (150 + 250)
+sum3 = sum2 + sum2   # 800 (400 + 400)
+
+#Python divides the operators in the following groups:
+#Arithemetic operations
+a=5
+b=3
+print(a+b)  #Addition
+print(a-b)  #Subtraction
+print(a*b)  #Multiplication
+print(a%b)  #Modulus
+print(a/b)  #Division
+print(a//b) #Floor division
+print(a**b) #Exponentiation
+ 
+#string+Arithmetic
+#String repetition 
+s="pavani "
+print(s*3)
+
+#BOOLEAN + ARITHMETIC
+print(True * 10) #True ,since True=1
+print(False * 10) #false ,since False=0
+
+print(True + True) #2
+print(False + 5)  #5
+
+#COMPLEX NUMBER ARITHMETIC
+a = 2 + 3j
+b = 1 + 2j
+print(a + b)
+
+#Complex multiplication
+x = 1 + 1j
+y = 2
+print(x * y)
+
+#Complex power
+z = 2 + 0j
+print(z ** 2)
+
+#
+s="5"
+print(s*2)
+
+
+
+
+
+
+
+
+
+
+
+
+
