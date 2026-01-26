31f134e1f2ad45697a8ce6dc93bf199dd9f45aa3
V kids <pavanialuri1775@gmail.com>
2026-01-26 10:36:33 +0530

    Added day7 programs.py

---
+++ day7/problems.py
@@ -0,0 +1,12 @@
+print(bool(3.14))#True
+print(bool(0.0)) #False
+print("python" > "java")  #True
+#How Python compares strings:
+'''Python compares strings character by character using ASCII values.
+Comparison stops as soon as it finds a difference.'''
+
+print(5 > 3 == 3 < 4)#True
+print(True==1) #True
+print(False==0)#True 
+
+print(not(True or False)) #False
