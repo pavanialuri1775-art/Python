4db96a6818feb0949d3f688acb3572b8cd8eead8
Pavani Aluri <pavanialuri1775@gmail.com>
2026-01-30 06:19:41 +0530

    Added day12 loops programs

---
+++ day12/loops.py
@@ -0,0 +1,24 @@
+#Loop Through a List:we can loop through the list using a for loop.
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+  
+#Loop Through the Index Numbers
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+
+#Using a While Loop:
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+  
+  #List Comprehension offers the shortest syntax for looping through lists
+#A short hand for loop that will print all items in a list:
+
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
+
+
