4f7d4c3685162bf01d35369a61d21902f89456af
Pavani Aluri <pavanialuri1775@gmail.com>
2026-02-03 16:55:55 +0530

    Added real world problems on lists

---
+++ day16/real.py
@@ -0,0 +1,69 @@
+#shopping cart items
+'''cart = ["milk","bread","eggs","butter"]
+print(cart[0]) #milk
+print(cart[-1]) #butter
+cart.append("cheese") 
+cart.remove("bread")
+print(cart) #['milk','eggs','butter','cheese']
+
+#Student Subjects List
+subjects=["Math","English","Science"]
+subjects.insert(1,"computer")
+print(subjects)
+subjects.remove("English")
+print(subjects)#['Math', 'computer', 'Science']
+
+#Favorite Movies Collection
+#Print the second movie.
+movies = ["Avatar","Inception","Interstellar","Titanic"]
+print(movies[1]) #Inception
+#Print the last two movies using slicing.
+print(movies[-2:])# ['Interstellar', 'Titanic']
+#Reverse the movie list using slicing.
+print(movies[::-1])#['Titanic', 'Interstellar', 'Inception', 'Avatar']
+
+#Phone Contacts
+contacts=["Mom","Dad","Friend","Teacher"]
+
+#Change "Friend" to "Best Friend".
+contacts[2]="Best Friend"
+
+#Copy the contacts list into backup
+backup=contacts.copy()
+print(backup)
+
+#Remove the last contact from backup.
+backup.pop(-1)
+print(backup)'''
+
+#Cricket Team Players
+players = ["Rohit","Virat","Dhoni","Hardik"]
+#Print the captain (first player)
+print("captian:",players[0]) #captian: Rohit
+
+#Add "Jadeja" to the team.
+players.append("jadeja")
+
+#Sort the team names alphabetically
+players.sort()
+print(players)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
