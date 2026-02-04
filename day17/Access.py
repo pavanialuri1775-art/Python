80b704c37253837e35014024c14930c789904e6c
Pavani Aluri <pavanialuri1775@gmail.com>
2026-02-04 16:42:34 +0530

    Added access program

---
+++ day17/Access.py
@@ -0,0 +1,27 @@
+#You can access tuple items by referring to the index number, inside square brackets
+#Print the second item in the tuple:
+'''thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])#banana
+
+#Negative Indexing
+#Negative indexing means start from the end.
+#Print the last item of the tuple
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])#cherry'''
+
+#Check if Item Exists
+#To determine if a specified item is present in a tuple use the in keyword
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")#Yes, 'apple' is in the fruits tuple
+
+
+#Use a range of indexes to print the third, fourth, and fifth item in the tuple.
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
+
+
+
+
+
+
