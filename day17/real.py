98393108d27392b29b4da2cdc22079d2a05adac6
Pavani Aluri <pavanialuri1775@gmail.com>
2026-02-04 23:58:17 +0530

    Added real programs

---
+++ day17/real.py
@@ -0,0 +1,24 @@
+#Electricity Bill Calculation
+'''units=int(input("enter number of units:"))
+rate=int(input("rate:"))
+bill=units*rate
+GST=18/100*bill
+bill+=GST
+print("Total bill:",bill)
+
+#Check if a Room Number is Even or Odd
+n=int(input("Enter a number:"))
+if (n&1)==1:
+    print("leftside")
+else:
+    print("Right side")
+    
+    
+#Check if a number is divisible by 4 using bitwise (No %)
+n=int(input("Enter a number:"))
+print((n&3)==0)'''
+
+#Check if number is divisible by 8 using bitwise
+n=int(input("enter a number:"))
+print(n&7==0)
+
