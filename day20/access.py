de2f8e3c6eff40c61b89ec62ea7c1e573baf4f92
Pavani Aluri <pavanialuri1775@gmail.com>
2026-02-07 05:18:51 +0530

    Added access

---
+++ day20/access.py
@@ -0,0 +1,14 @@
+#we cannot access an element in set index so we use in key word to check if an element exists
+s={1,2,3}
+print(2 in s)
+
+#Check if "banana" is NOT present in the set:
+thisset={"apple","banana","cherry"}
+print("banana" not in thisset)
+
+#we can loop through the set items using a for loop.
+thisset={"apple","banana","cherry"}
+for x in thisset:
+    print(x)
+    
+#
