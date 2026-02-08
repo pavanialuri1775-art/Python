0d63148880691daa2442967a7a0634c0b57f3aff
Pavani Aluri <pavanialuri1775@gmail.com>
2026-02-08 12:43:12 +0530

    Added

---
+++ day21/remove.py
@@ -0,0 +1,25 @@
+#To remove an item in a set, use the remove(), or the discard() method.
+'''thisset = {"apple", "banana", "cherry"}
+thisset.remove("banana")
+print(thisset)#{'apple', 'cherry'}
+
+#Remove "banana" by using the discard() method
+thisset = {"apple", "banana", "cherry"}
+thisset.discard("banana")
+print(thisset)#{'apple', 'cherry'}
+
+#The clear() method empties the set
+thisset = {"apple", "banana", "cherry"}
+thisset.clear()
+print(thisset)'''
+
+#The del keyword will delete the set completely
+thisset={"apple", "banana", "cherry"}
+del thisset
+print(thisset)
+
+
+
+
+
+
