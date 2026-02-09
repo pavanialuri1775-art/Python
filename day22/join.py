9d8682a224ba4a5ef66da4b3fb89994ae5426ea6
Pavani Aluri <pavanialuri1775@gmail.com>
2026-02-09 22:16:39 +0530

    Added

---
+++ day22/join.py
@@ -0,0 +1,19 @@
+#There are several ways to join two or more sets in Python.
+#The union() and update() methods joins all items from both sets.
+#The intersection() method keeps ONLY the duplicates.
+#The difference() method keeps the items from the first set that are not in the other set(s).
+#The symmetric_difference() method keeps all items EXCEPT the duplicates.
+
+
+#Union
+#The union() method returns a new set with all items from both sets.
+#Join set1 and set2 into a new set:
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = set1.union(set2)
+print(set3)
+
+
+
+
+
