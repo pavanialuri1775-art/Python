0a686d8ff0319c0b282d0d9d319d9714589b4504
Pavani Aluri <pavanialuri1775@gmail.com>
2026-02-12 18:42:43 +0530

    Added

---
+++ day24/list.py
@@ -0,0 +1,68 @@
+#append()
+li=[1,2,3]
+li.append(4)
+print(li)
+
+#extend()
+a=[1,2,3]
+b=[4,5,6]
+a.extend(b)
+print(a)
+
+#insert()
+numbers=[10,20,30]
+numbers.insert(1,100)
+print(numbers)
+
+#remove()
+numbers=[10,20,30,40]
+numbers.remove(20)
+print(numbers)
+
+#pop()
+nums=[1,2,3,4]
+nums.pop(0)
+print(nums)
+
+#clear()
+data=[5, 6, 7]
+data.clear()
+print(data)
+
+#index()
+data=[1,2,3]
+print(data.index(2))
+
+#count()
+nums=[1, 2, 2, 3, 2, 4]
+print(nums.count(2))
+
+#sort()
+nums=[5, 1, 4, 2]
+nums.sort()
+print(nums)
+
+#reverse()
+nums=[1, 2, 3, 4]
+nums.reverse()
+print(nums)
+
+#copy()
+nums=[10,20,30]
+nums.copy()
+print(nums)
+
+#len()
+data= [1, 2, 3, 4, 5]
+print(len(data))
+
+#max() and min()
+nums=[10,5,8,20]
+print(max(nums))
+print(min(nums))
+
+#sum()
+nums=[1,2,3,4]
+print(sum(nums))
+
+
