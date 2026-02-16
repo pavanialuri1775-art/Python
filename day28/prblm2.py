2dc50f92d252fbb8f22947b20a1bd305be147807
Pavani Aluri <pavanialuri1775@gmail.com>
2026-02-16 12:31:13 +0530

    Added

--- day28/prblm2.py
+++ day28/prblm2.py
@@ -5,13 +5,13 @@ d={
     "d":20,
     "e":15
 }
-print(max(d,key=d.get))
+print(max(d,key=d.get))#d
 new_dict={}
 for x,y in d.items():
     if y not in new_dict:
         new_dict[y]=[x]
     else:
-        new_dict[y].append(x)
+        new_dict[y].append(x)#{10: ['a', 'c'], 15: ['b', 'e'], 20: ['d']}
 print(new_dict)
 count_dict={}
 for v in d.values():
@@ -19,11 +19,11 @@ for v in d.values():
         count_dict[v]=1
     else:
         count_dict[v]+=1
-print(count_dict)
+print(count_dict)#{10: 2, 15: 2, 20: 1}
 new_dict={}
 for v in d:
     d[v]**=2
-print(d)
+print(d)#{'a': 100, 'b': 225, 'c': 100, 'd': 400, 'e': 225}
 d={
     "a":10,
     "b":15,
@@ -35,5 +35,5 @@ l_dict={}
 for k,v in d.items():
     if v<=15:
         l_dict[k]=v
-print(l_dict)
+print(l_dict)#{'a': 10, 'b': 15, 'c': 10, 'e': 15}
 
