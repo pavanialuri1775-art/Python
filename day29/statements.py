2ad065e7557cb010aea411860eace1b5a16c1c78
Pavani Aluri <pavanialuri1775@gmail.com>
2026-02-17 18:18:35 +0530

    updated code

--- day29/statements.py
+++ day29/statements.py
@@ -11,10 +11,24 @@ Greater than or equal to: a >= b'''
 #checking if a number is positive
 number = 20
 if number > 0:
-  print("The number is positive")
+  print("The number is positive")#The number is positive
 
 #checking if b is greater than a
 a = 33
-b = 20
+b = 200
 if b > a:
-  print("b is greater than a")
+  print("b is greater than a")#b is greater than a
+  
+#if statement raises an error without indentation 
+#Multiple Statements in If Block:we can have multiple statements inside a if block and all must be indented at same level.
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+  
+#using variables in conditions
+#Boolean variables can be used directly in if statements without comparison operators
+is_student=True
+if is_student:
+    print("i am a student")
