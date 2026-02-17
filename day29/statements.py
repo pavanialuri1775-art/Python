6c86eb6f47b380682fe08abbf92ae50212c8eab3
Pavani Aluri <pavanialuri1775@gmail.com>
2026-02-17 17:47:42 +0530

    Added

---
+++ day29/statements.py
@@ -0,0 +1,20 @@
+#Python supports the usual logical conditions from mathematics
+'''Equals: a == b
+Not Equals: a != b
+Less than: a < b
+Less than or equal to: a <= b
+Greater than: a > b
+Greater than or equal to: a >= b'''
+#we use these conditions in several ways most commonly in if statements
+#if statement evaluates the condition and if the condition is true the code inside the if block is executed else If the condition is false, the code block is skipped.
+
+#checking if a number is positive
+number = 20
+if number > 0:
+  print("The number is positive")
+
+#checking if b is greater than a
+a = 33
+b = 20
+if b > a:
+  print("b is greater than a")
