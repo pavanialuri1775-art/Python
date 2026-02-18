d5fdcc5fe11bc4d16e62807e8cca10dda4ee8388
Pavani Aluri <pavanialuri1775@gmail.com>
2026-02-18 19:35:09 +0530

    Added

---
+++ day30/real.py
@@ -0,0 +1,24 @@
+#Electricity Bill calculator
+'''units=int(input("Enter number of units:"))
+if units<=100:
+    bill=units*1
+elif units<=200:
+    bill=units*2
+elif units<=300:
+    bill=units*3
+else:
+    bill=units*5
+print("total bill:",bill)'''
+
+#smart Traffic Light System
+signal=input("Enter input signal:")
+if signal=="red":
+    print("stop")
+elif signal=="yellow":
+    print("Ready")
+elif signal=="green":
+    print("go")
+else:
+    print("invalid signal")
+    
+    
