04c7d9cf8acb05d49bc1a0262f51e9167c0399b0
Pavani Aluri <pavanialuri1775@gmail.com>
2026-02-20 23:22:19 +0530

    Added

---
+++ day32/for.py
@@ -0,0 +1,14 @@
+#for loop is used to iterate over a sequence(list,tuple,string)
+#example
+#print name of each person in a student list
+names=["pavani","ammulu","puppy"]
+for x in names:
+    print(x)
+    
+#Looping Through a String
+#Even strings are iterable objects, they contain a sequence of characters
+#example
+#Loop through the letters in the word "banana"
+for x in "banana":
+    print(x)
+    
