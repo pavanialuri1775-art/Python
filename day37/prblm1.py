b534797769c53a09178bc34155be566b96fab67f
Pavani Aluri <pavanialuri1775@gmail.com>
2026-02-25 11:03:42 +0530

    Added

---
+++ day37/prblm1.py
@@ -0,0 +1,33 @@
+#square pattern
+for i in range(1,6):
+    print("*"*5)
+    
+#number increasing pattern
+n=5
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()
+    
+#Right triangle star pattern
+n=5
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print("*",end="")
+    print()
+    
+
+    
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
