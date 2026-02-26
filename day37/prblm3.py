2e6e2948d75322b2d59e91cd33fff832bc9bb93c
Pavani Aluri <pavanialuri1775@gmail.com>
2026-02-26 18:29:39 +0530

    Added

---
+++ day37/prblm3.py
@@ -0,0 +1,18 @@
+'''#print horizontal stars
+for i in range(1,6):
+    print("*",end="")#*****'''
+    
+#print vertical rows
+for i in range(1,6):
+    print("*")
+    
+#right angle pattern
+for i in range(1,6):
+    print("*"*i)
+    
+    
+#inverted right angle triangle
+for i in range(5,0,-1):
+    print("*"*i)
+
+
