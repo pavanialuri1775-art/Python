db78df0486e95f76da58905a9e69ab5195e4c31e
Pavani Aluri <pavanialuri1775@gmail.com>
2026-03-09 19:46:22 +0530

    Added

---
+++ day44/prblm2.py
@@ -0,0 +1,14 @@
+# Print list in reverse order using a loop
+'''list1=[10, 20, 30, 40, 50]
+for i in range(len(list1)-1,-1,-1):
+    print(list1[i])
+    
+#Display numbers from -10 to -1 using for loop
+for i in range(-10,0):
+    print(i)'''
+    
+#Display a message “Done” after the successful execution of the for loop
+for i in range(1,10):
+    print(i)
+else:
+    print("done!")
