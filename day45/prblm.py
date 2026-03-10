61754f44a00536610f083de131abbb38e839cad1
Pavani Aluri <pavanialuri1775@gmail.com>
2026-03-10 19:48:19 +0530

    Added

---
+++ day45/prblm.py
@@ -0,0 +1,21 @@
+#reversing a number
+'''num = input("Enter a number: ")
+rev = ""
+for i in num:
+    rev = i + rev
+print("Reversed number:", rev)
+
+#Print elements from a given list present at odd index positions
+my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+for i in my_list[1::2]:
+    print(i)
+    
+#Calculate the cube of all numbers from 1 to a given number
+n=int(input("enter a number:"))
+for i in range(1,n+1):
+       print(i**3)'''
+       
+#Print Full Multiplication Table
+n=int(input("enter a number:"))
+for i in range(1,11):
+    print(i*n,end=" ")
