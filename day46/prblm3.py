f183268c6878c8a61565e7a6fa3fe9bacf8bb3e3
Pavani Aluri <pavanialuri1775@gmail.com>
2026-03-11 09:51:50 +0530

    day46

---
+++ day46/prblm3.py
@@ -0,0 +1,15 @@
+#Typecasting + Loops
+#Input a number as string and calculate sum of digits
+'''n=input("enter a number:")#1234
+sum=0
+for i in n:
+    sum+=int(i)
+print(sum)#10'''
+
+#Convert a list of string numbers to integers using loop.
+lst = ["1","2","3","4"]
+result=[]
+for i in lst:
+    result.append(int(i))
+print(result)
+    
