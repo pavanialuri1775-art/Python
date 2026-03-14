a91cc62af24bf934d9b9f5fd14835353ccf0e425
Pavani Aluri <pavanialuri1775@gmail.com>
2026-03-14 09:17:18 +0530

    Added

---
+++ day49/prblm1.py
@@ -0,0 +1,30 @@
+#1
+'''i=1
+while i<=10:
+    if i==6:
+        break
+    print(i,end=" ")
+    i+=1
+    
+#2
+i=1
+while i<=10:
+    if i==5:
+        i+=1
+        continue
+    print(i,end=" ")
+    i+=1'''
+    
+#3
+i=1
+while i<=15:
+    if i%4==0:
+        i+=1
+        continue
+    if i==13:
+        break
+    print(i,end=" ")
+    i+=1
+    
+    
+    
