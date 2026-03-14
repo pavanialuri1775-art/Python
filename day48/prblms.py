68a0d4e2b98cb640e0b1f0f2aa1aefe61840a1d1
Pavani Aluri <pavanialuri1775@gmail.com>
2026-03-14 13:00:05 +0530

    Added

---
+++ day48/prblms.py
@@ -0,0 +1,20 @@
+#for the given list of strings find a str which is a palindromic string
+'''thelist=["pavani","afifa","lavss","madam","racecar"]
+for word in thelist:
+    if word==word[::-1]:
+        print(word)'''
+
+
+#take a list str and cnt the vowels in each str,  if the cnt of vowels is even print the str
+thelist=["apple","pavss","banana","durga"]
+for word in thelist:
+    vowels="AEIOUaeiou"
+    count=0
+    for ch in word:
+        if ch in vowels:
+            count+=1
+    if count%2==0:
+        print(word)
+        
+    
+        
