60458dc3eab17f87b48f41646b516c82a2265964
Pavani Aluri <pavanialuri1775@gmail.com>
2026-03-15 08:56:17 +0530

    Added

---
+++ day50/func1.py
@@ -0,0 +1,35 @@
+#1
+#say hello(no input and no output)
+def say_hello():
+    print("Hello,Pavani!")
+say_hello()#Hello,Pavani!
+
+#2
+#greet someone(input,no output)
+def greet(name):
+    print(f"hello,{name}!")
+greet("pavss")#hello,pavss!
+greet("pavani")#hello,pavss!
+
+#3
+#Favourite number
+def favourite_number():
+    return 2
+num=favourite_number()
+print(num)
+
+#4
+#add two numbers
+def add(a,b):
+    return a+b
+result=add(3,7)
+print(result)
+
+#5
+#square a number
+def square(n):
+    return n*n
+result=square(4)
+print(result)
+
+
