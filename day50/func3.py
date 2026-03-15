4c9858231fb0520ae938183f020ca720dbd7a2a2
Pavani Aluri <pavanialuri1775@gmail.com>
2026-03-15 09:45:53 +0530

    Added

---
+++ day50/func3.py
@@ -0,0 +1,19 @@
+#11
+# convert temperatures from Fahrenheit to Celsius
+'''def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit-32)*5/9
+num=float(input("enter temperature in fahrenheit:"))
+celsius=fahrenheit_to_celsius(num)
+print(celsius)'''
+
+#12
+def my_function(name):
+    print(name+" aluri")
+my_function("Pavani")#Pavani aluri
+
+
+#13
+def my_function(fname,lname):
+    print(fname+" "+lname)
+my_function("Pavani","Aluri")
+    
