eb1bba1d75604182c2dfd81598bc7252d308c18e
Pavani Aluri <pavanialuri1775@gmail.com>
2026-03-16 19:27:18 +0530

    Added

---
+++ day51/funct1.py
@@ -0,0 +1,13 @@
+#1
+#positional arguments
+def introduce(name,city):
+    print("My name is"+" "+name)
+    print("I live in"+" "+city)
+introduce("Pavani","Hyderabad")
+
+#2
+def pet_info(animal,name):
+    print("I have a"+" "+animal)
+    print("My dog's name is",name)
+pet_info("dog","buddy")
+    
