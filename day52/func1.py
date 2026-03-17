f1bd8259ff7869c54e222e970ee085916eda3f4c
Pavani Aluri <pavanialuri1775@gmail.com>
2026-03-17 17:54:39 +0530

    Added

---
+++ day52/func1.py
@@ -0,0 +1,16 @@
+#return values
+def multiply(a,b):
+    return a*b
+a=int(input("enter a number:"))#5
+b=int(input("enter a number:"))#6
+result=multiply(a,b)
+print(result)#30
+
+#
+def get_colors():
+    return ["red","blue","green"]
+my_list=get_colors()
+print(my_list)#['red', 'blue', 'green']
+print(my_list[1])
+
+
