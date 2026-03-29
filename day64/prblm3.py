7ba4b16e15665cec481c3a84a1da1dc9a5615a61
Pavani Aluri <pavanialuri1775@gmail.com>
2026-03-29 10:20:16 +0530

    added

---
+++ day64/prblm3.py
@@ -0,0 +1,35 @@
+#13 Function with default argument
+def greet(name="Guest"):
+    return f"hello {name}"
+print(greet())
+
+#14 use *args
+#Find sum of any number of inputs
+def sum_of_nums(*args):
+    result=0
+    for i in args:
+        result+=i
+    return result
+print(sum_of_nums(2,3,4,5,6))
+
+#15 **kwargs
+def key_value(**kwargs):
+    return kwargs
+data=key_value(name="pavani",age=20)
+print(data)
+
+#16 use **kwargs
+#find sum of any numbers
+def sum_of_nums(**kwargs):
+    result=0
+    for value in kwargs.values():
+        result+=value
+    return result
+print(sum_of_nums(a=10,b=12,c=2))
+
+#17 maximum value using kwargs
+def maximum_value(**kwargs):
+    return  max(kwargs.values())
+print(maximum_value(a=10,b=50,c=30))
+    
+    
