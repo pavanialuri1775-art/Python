ab0d2bb9a05cfc688a77cd8267f4c3c8f4a8b70e
Pavani Aluri <pavanialuri1775@gmail.com>
2026-04-08 22:39:17 +0530

    Added

---
+++ day72/prblm3.py
@@ -0,0 +1,12 @@
+#Parse JSON
+import json
+data = '{"name": "Pavani", "age": 22, "city": "Hyderabad"}'
+y=json.loads(data)
+print(y["name"])
+print(y["city"])
+
+#convert DICT->JSON
+import json
+d={"course":"python","duration":"3 months"}
+s=json.dumps(d)
+print(s)
