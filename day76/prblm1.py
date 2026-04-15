2293d9ff3b7ff0139b9d8484a95a3d247e4730d2
Pavani Aluri <pavanialuri1775@gmail.com>
2026-04-15 18:32:46 +0530

    Added

---
+++ day76/prblm1.py
@@ -0,0 +1,25 @@
+#add 
+def add(a,b):
+    return a+b
+n,m=map(int,input().split())
+print(add(n,m))
+
+#
+def sub(a,b):
+    return a-b
+n,m=map(int,input().split())
+print(sub(n,m))
+
+#
+def mul(a,b):
+    return a*b
+n,m=map(int,input().split())
+print(mul(n,m))
+
+#
+def div(a,b):
+    return a-b
+n,m=map(int,input().split())
+print(div(n,m))
+
+
