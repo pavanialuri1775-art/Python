7cafeb726806aa1c0ec68c0992a93bf9afdb6b07
Pavani Aluri <pavanialuri1775@gmail.com>
2026-04-18 21:21:32 +0530

    Added

--- day78/prblm2.py
+++ day78/prblm2.py
@@ -12,8 +12,8 @@ else:
 #The finally block, if specified, will be executed regardless if the try block raises an error or not
 # 
 try:
-  print(x)
-except:
+  #print(x)
+#except:
   print("Something went wrong")
 finally:
   print("The 'try except' is finished")
