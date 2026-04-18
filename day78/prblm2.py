9d2211902fa672e4bb0b1faa9d16d3c73e4c4f2e
Pavani Aluri <pavanialuri1775@gmail.com>
2026-04-18 21:21:02 +0530

    Added

---
+++ day78/prblm2.py
@@ -0,0 +1,19 @@
+#Else:
+#we can use the else keyword define a block of code to be executed if no errors were raised.
+#In this example, the try block does not generate any error.
+try:
+  print("Hello")
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong")
+  
+#Finally
+#The finally block, if specified, will be executed regardless if the try block raises an error or not
+# 
+try:
+  print(x)
+except:
+  print("Something went wrong")
+finally:
+  print("The 'try except' is finished")
