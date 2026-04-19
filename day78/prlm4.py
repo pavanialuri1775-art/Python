bcf16a832f1c14b7911bfdd7fe30a7a12dbdc17b
Pavani Aluri <pavanialuri1775@gmail.com>
2026-04-19 20:12:43 +0530

    Added

---
+++ day78/prlm4.py
@@ -0,0 +1,14 @@
+#Divide Two Numbe
+'''a,b=map(int,input().split())
+try:
+    print(a/b)
+except ZeroDivisionError:
+    print("cannot divide by zero")'''
+    
+#Integer Input Validation
+try:
+    n=int(input())
+    print("valid")
+except:
+    print("Invalid")
+    
