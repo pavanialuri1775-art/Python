51e911671860ef0994c3f94a7cc850ce770d9d8a
Pavani Aluri <pavanialuri1775@gmail.com>
2026-04-21 19:47:48 +0530

    added

---
+++ day80/prblm4.py
@@ -0,0 +1,22 @@
+#Print 0 to 9
+for i in range(10):
+    print(i,end=" ")
+    
+#print 1 to n 
+n=int(input("enter any num:"))
+for i in range(1,n+1):
+    print(i,end=" ")
+    
+#print even numbers from 2 to 20
+for i in range(2,21,2):
+    print(i,end=" ")
+    
+#print odd numbers from 1 to 15
+for i in range(1,15+1,2):
+    print(i,end=" ")
+
+#print multiplication table
+n=int(input("enter any number:"))
+for i in range(11):
+    print(n,"*",i,"=",n*i)
+
