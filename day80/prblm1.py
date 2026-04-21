ebb5967b212bbc2f820138fd8e5a482f62625d92
Pavani Aluri <pavanialuri1775@gmail.com>
2026-04-21 08:52:50 +0530

    added

---
+++ day80/prblm1.py
@@ -0,0 +1,14 @@
+#What is an Array?
+#An array can hold many values under a single name, and you can access the values by referring to an index number.
+
+#Access the Elements of an Array
+#we refer to an array element by referring to the index number.
+
+#The Length of an Array
+#the len() method to return the length of an array (the number of elements in an array).
+
+#
+cars=["Ford", "Volvo", "BMW"]
+print(cars[0])
+cars[1]="Toyota"
+print(cars)
