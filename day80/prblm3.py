00fdac984fd8a0a8c5a045d2c72840b4951ec90b
Pavani Aluri <pavanialuri1775@gmail.com>
2026-04-21 19:34:18 +0530

    Added

---
+++ day80/prblm3.py
@@ -0,0 +1,12 @@
+#Membership Testing (in)
+r = range(0,10,2)
+print(6 in r)#True
+print(7 in r)#False
+
+#Length of range
+r = range(0,10,2)
+print(len(r))
+
+#Negative Step
+for i in range(10,0,-1):
+    print(i)
