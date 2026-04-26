149a003a9ecde9773432df57058ec9633e2d9b51
Pavani Aluri <pavanialuri1775@gmail.com>
2026-04-26 12:24:18 +0530

    added

---
+++ day82/pr1.py
@@ -0,0 +1,8 @@
+#Python PIP
+#PIP is a package manager for Python packages, or modules if you like.
+#What is a Package?
+#A package contains all the files you need for a module.
+#Modules are Python code libraries you can include in your project.
+#Using a Package
+#Once the package is installed, it is ready to use.
+
