00bfceac370c1ee3d7a735a01cfaf59ae4dfb7de
Pavani Aluri <pavanialuri1775@gmail.com>
2026-04-27 20:36:56 +0530

    Added

---
+++ day83/prblm1.py
@@ -0,0 +1,15 @@
+#number of steps to reduce a number to zero
+def numberOfSteps(num):
+        steps=0
+        while num>0:
+            if num%2==0:
+                num=num//2
+            else:
+                num=num-1
+            steps+=1
+        return steps
+print(numberOfSteps(14))
+
+
+             
+        
