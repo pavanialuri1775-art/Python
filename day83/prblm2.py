57bab33e0c303f4c889c455b488563985f4669d7
Pavani Aluri <pavanialuri1775@gmail.com>
2026-04-28 19:59:40 +0530

    added

---
+++ day83/prblm2.py
@@ -0,0 +1,5 @@
+#String Formatting
+#F-string allows us to format selected parts of a string.
+#Create an f-string
+txt = f"The price is 49 dollars"
+print(txt)
---
+++ day83/prblm3.py
@@ -0,0 +1,19 @@
+#ATM Withdrawal
+'''try:
+    total_amount=int(input("enter amount:"))
+    with_drawal=int(input("enter amount:"))
+    if with_drawal>total_amount:
+            raise ValueError("Insufficient Balance")
+    print("Remaining Balance:",total_amount-with_drawal)
+except ValueError as e:
+    print(e)'''
+    
+#Bank PIN Validation
+try:
+    pin=int(input("enter pin:"))
+    if pin==1234:
+        print("Valid Pin")
+    else:
+        raise ValueError("Wrong PIN")
+except ValueError as e:
+    print(e)
