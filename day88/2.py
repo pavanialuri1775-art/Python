d934b9360399bcae3016e81bb8922fc46951ee09
Pavani Aluri <pavanialuri1775@gmail.com>
2026-05-07 21:16:36 +0530

    Added

--- day88/2.py
+++ day88/2.py
@@ -8,3 +8,15 @@ class Solution:
 class Solution:
     def findDifference(self, nums1,nums2):
         return [list(set(nums1)-set(nums2)),list(set(nums2)-set(nums1))]
+    
+    
+class Solution:
+    def maximumValue(self, strs):
+        lst=[]
+        for word in strs:
+            if word.isdigit():
+                lst.append(int(word))
+            else:
+                lst.append(len(word))
+        return max(lst)
+                
