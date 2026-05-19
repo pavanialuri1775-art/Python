c060600dee7279550103e001c4c2a47fbd3105c4
Pavani Aluri <pavanialuri1775@gmail.com>
2026-05-19 22:21:30 +0530

    Added

---
+++ day95/all.py
@@ -0,0 +1,41 @@
+#3
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius * self.radius
+
+
+class Rectangle(Shape):
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
+    def area(self):
+        return self.length * self.breadth
+class Triangle(Shape):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+    def area(self):
+        return 0.5 * self.base * self.height
+c = Circle(5)
+r = Rectangle(4, 6)
+t = Triangle(10, 8)
+print(c.area())
+print(r.area())
+print(t.area())
+
+
+        
